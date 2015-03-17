a8da117aa40482214d26391de7376a1e28fce089
Matthew Egan <matthewj.egan@hotmail.com>
2015-03-17 13:26:34 +1100

    Added Game class

---
+++ Game.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+class Game:
+    def __init__(self, team1, team1players, team2, team2players, date):
+        self.team1 = team1
+        self.team1players = team1players
+        self.team2 = team2
+        self.team2players = team2players
+        self.date = date
+
+        self.team1Score = 0
+        self.team2Score = 0
+
+        def addGoal(self, team, player):
+            # Add goal to score
+            if team == self.team1: 
+                self.team1Score += 1
+            elif team == self.team2: 
+                self.team2Score += 1
+
+            # Add goal to player
+            player.addGoal()
+
+        def getTeam1Players(self): return self.team1players
+        def getTeam2Players(self): return self.team2players
+        def getTeam1(self): return self.team1
+        def getTeam2(self): return self.team2
+        def getScore(self):
+            return self.team1 + ": " + str(self.team1Score) + " - " + str(self.team2Score) + " :" + self.team2
